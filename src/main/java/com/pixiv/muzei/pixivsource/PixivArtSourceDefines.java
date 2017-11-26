823db841fd3ec40bd30fc1d84dea00e185940d1e
Sunguk Lee <d3m3vilurr@gmail.com>
2017-11-26 19:39:55 +0900

    Fix feed update problems
    now a day, pixiv apis fully require authorization.
    so, I decided partial reverting 4334774206
    
    this change also require to split some json parse sections and
    detection original image urls.
    
    this patch looks little mess, we should refactoring later.
    
    Resolve #17

--- src/main/java/com/pixiv/muzei/pixivsource/PixivArtSourceDefines.java
+++ src/main/java/com/pixiv/muzei/pixivsource/PixivArtSourceDefines.java
@@ -11,15 +11,20 @@ public class PixivArtSourceDefines {
     public static final String CLIENT_SECRET = "HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK";
 
     // urls
+    public static final String PIXIV_HOST = "https://www.pixiv.net";
     private static final String PIXIV_API_HOST = "https://app-api.pixiv.net";
+    private static final String PIXIV_UNAUTH_RANKING_URL = PIXIV_HOST + "/ranking.php";
     private static final String PIXIV_RANKING_URL = PIXIV_API_HOST + "/v1/illust/ranking";
 
+    public static final String MEMBER_ILLUST_URL =
+            PIXIV_HOST + "/member_illust.php?mode=medium&illust_id=";
+
     public static final String DAILY_RANKING_URL =
-            PIXIV_RANKING_URL + "?mode=day";
+            PIXIV_UNAUTH_RANKING_URL + "?mode=daily&format=json";
     public static final String WEEKLY_RANKING_URL =
-            PIXIV_RANKING_URL + "?mode=week";
+            PIXIV_UNAUTH_RANKING_URL + "?mode=weekly&format=json";
     public static final String MONTHLY_RANKING_URL =
-            PIXIV_RANKING_URL + "?mode=month";
+            PIXIV_UNAUTH_RANKING_URL + "?mode=monthly&format=json";
     public static final String BOOKMARK_URL =
             PIXIV_API_HOST + "/v1/user/bookmarks/illust";
     public static final String FOLLOW_URL =
