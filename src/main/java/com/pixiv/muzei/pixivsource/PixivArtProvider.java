67f2a3b19bbe2847bc954a5611c4532ea26a1ec9
Sunguk Lee <d3m3vilurr@gmail.com>
2019-05-05 06:53:33 +0900

    Move to muzei3 API
    resolve #25

--- src/main/java/com/pixiv/muzei/pixivsource/PixivArtProvider.java
+++ src/main/java/com/pixiv/muzei/pixivsource/PixivArtProvider.java
@@ -1,28 +1,7 @@
-/*
- * Copyright 2014 Hong Minhee
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.pixiv.muzei.pixivsource;
 
-import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
@@ -32,17 +11,15 @@ import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonArray;
 import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
-import com.google.android.apps.muzei.api.Artwork;
-import com.google.android.apps.muzei.api.RemoteMuzeiArtSource;
+import com.google.android.apps.muzei.api.provider.Artwork;
+import com.google.android.apps.muzei.api.provider.MuzeiArtProvider;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.concurrent.TimeUnit;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -52,60 +29,73 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class PixivArtSource extends RemoteMuzeiArtSource {
-    private static final String LOG_TAG = "muzei.PixivArtSource";
-    private static final String SOURCE_NAME = "PixivArtSource";
-    private static final int MINUTE = 60 * 1000;  // a minute in milliseconds
+public class PixivArtProvider extends MuzeiArtProvider {
+    private static final String LOG_TAG = "muzei.PixivArtProvider";
+    private static final String PROVIDER_NAME = "PixivArtProvider";
 
     private String accessToken = null;
     private String userId = null;
     private boolean authorized = false;
 
-    public PixivArtSource() {
-        super(SOURCE_NAME);
-    }
-
     @Override
-    public void onCreate() {
-        super.onCreate();
-        setUserCommands(BUILTIN_COMMAND_ID_NEXT_ARTWORK);
-    }
-
-    private int getChangeInterval() {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        final String s = preferences.getString(
-                "pref_changeInterval", String.valueOf(R.string.pref_changeInterval_default)
-        );
-        Log.d(LOG_TAG, "pref_changeInterval = " + s);
+    protected void onLoadRequested(boolean initial) {
+        final JsonObject updateUriInfo = getUpdateUriInfo();
+        final JsonObject ranking;
+        final JsonArray contents;
 
         try {
-            return Integer.parseInt(s);
-        } catch (NumberFormatException e) {
-            Log.w(LOG_TAG, e.toString(), e);
-            return 0;
+            Response resp = sendGetRequest(updateUriInfo);
+            if (!resp.isSuccessful()) {
+                return;
+            }
+
+            ranking = Json.parse(resp.body().string()).asObject();
+            contents = getContents(ranking);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, e.toString());
+            return;
         }
-    }
 
-    private boolean isOnlyUpdateOnWifi() {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        final boolean defaultValue = false,
-                      v = preferences.getBoolean("pref_onlyWifi", defaultValue);
-        Log.d(LOG_TAG, "pref_onlyWifi = " + v);
-        return v;
-    }
+        Log.d(LOG_TAG, "The number of Contents: " + contents.size());
 
-    private boolean isEnabledWifi() {
-        ConnectivityManager connectivityManager =
-            (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        return wifi.isConnected();
-    }
+        if (contents.isEmpty()) {
+            Log.w(LOG_TAG, "No artworks returned from Pixiv");
+            return;
+        }
 
-    private void scheduleUpdate() {
-        final int changeInterval = getChangeInterval();
+        for (JsonValue item : contents) {
+            final JsonObject content = item.asObject();
+            final int illustId = getIllustId(content);
+            final int restrict = getRestrictMode(content);
 
-        if (changeInterval > 0) {
-            scheduleUpdate(System.currentTimeMillis() + changeInterval * MINUTE);
+            if (illustId < 0 || restrict < 0) {
+                continue;
+            }
+
+            final String token = illustId + "." + restrict;
+            Log.d(LOG_TAG, token);
+
+            final String workUri = PixivArtSourceDefines.MEMBER_ILLUST_URL + illustId;
+            final Uri webUri = Uri.parse(workUri);
+            final Uri fileUri;
+            try {
+                fileUri = downloadOriginalImage(content, token, updateUriInfo.getString("url", ""));
+            } catch (IOException e) {
+                Log.d(LOG_TAG, e.toString());
+                continue;
+            }
+            final String title = content.getString("title", "");
+
+            final String username = getUserName(content);
+
+            final Artwork artwork = new Artwork.Builder()
+                    .title(title)
+                    .byline(username)
+                    .webUri(webUri)
+                    .persistentUri(fileUri)
+                    .token(token)
+                    .build();
+            addArtwork(artwork);
         }
     }
 
@@ -113,7 +103,7 @@ public class PixivArtSource extends RemoteMuzeiArtSource {
         // cleanup authorization information
         this.authorized = false;
 
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
         if (!preferences.getBoolean("pref_useAuth", false)) {
             return false;
         }
@@ -156,7 +146,7 @@ public class PixivArtSource extends RemoteMuzeiArtSource {
     }
 
     private JsonObject getUpdateUriInfo() {
-        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
 
         final String updateMode = preferences.getString(
                 "pref_updateMode", String.valueOf(R.string.pref_updateMode_default)
@@ -192,96 +182,6 @@ public class PixivArtSource extends RemoteMuzeiArtSource {
                 ret.add("url", PixivArtSourceDefines.DAILY_RANKING_URL);
         }
         return ret;
-    }
-
-    @Override
-    protected void onTryUpdate(final int reason) throws RetryException {
-        final Artwork prevArtwork = getCurrentArtwork();
-        final String prevToken = prevArtwork != null ? prevArtwork.getToken() : null;
-        final JsonObject ranking;
-        final JsonArray contents;
-
-        if (isOnlyUpdateOnWifi() && !isEnabledWifi()) {
-            Log.d(LOG_TAG, "no wifi");
-            scheduleUpdate();
-            return;
-        }
-
-        final JsonObject updateUriInfo = getUpdateUriInfo();
-
-        try {
-            Response resp = sendGetRequest(updateUriInfo);
-            if (!resp.isSuccessful()) {
-                throw new RetryException();
-            }
-
-            ranking = Json.parse(resp.body().string()).asObject();
-            contents = getContents(ranking);
-        } catch (IOException e) {
-            Log.e(LOG_TAG, e.toString());
-            throw new RetryException(e);
-        }
-
-        Log.d(LOG_TAG, "The number of Contents: " + contents.size());
-
-        if (contents.isEmpty()) {
-            Log.w(LOG_TAG, "No artworks returned from Pixiv");
-            scheduleUpdate();
-            return;
-        }
-
-        final Random random = new Random();
-        while (true) {
-            final int i = random.nextInt(contents.size());
-            final JsonObject content;
-            try {
-                content = contents.get(i).asObject();
-            } catch (IndexOutOfBoundsException | NullPointerException e) {
-                Log.e(LOG_TAG, e.toString(), e);
-                throw new RetryException(e);
-            }
-
-            final int illustId = getIllustId(content);
-            final int restrict = getRestrictMode(content);
-
-            if (illustId < 0 || restrict < 0) {
-                continue;
-            }
-            final String token = illustId + "." + restrict;
-            Log.d(LOG_TAG, token);
-            if (prevToken != null && prevToken.equals(token)) {
-                continue;
-            }
-
-            final String workUri = PixivArtSourceDefines.MEMBER_ILLUST_URL + illustId;
-
-            final Uri fileUri = downloadOriginalImage(content, token, updateUriInfo.getString("url", ""));
-            final String title = content.getString("title", "");
-
-            final String username = getUserName(content);
-
-            final Artwork artwork = new Artwork.Builder()
-                .title(title)
-                .byline(username)
-                .imageUri(fileUri)
-                .token(token)
-                .viewIntent(new Intent(Intent.ACTION_VIEW, Uri.parse(workUri)))
-                .build();
-            publishArtwork(artwork);
-            break;
-        }
-
-        final Application app = getApplication();
-        if (app == null) {
-            Log.e(LOG_TAG, "getApplication() returns null");
-            throw new RetryException();
-        }
-        if (prevToken != null) {
-            final File file = new File(app.getExternalCacheDir(), prevToken);
-            file.delete();
-        }
-
-        scheduleUpdate();
     }
 
     private String getUserName(JsonObject content) {
@@ -331,24 +231,24 @@ public class PixivArtSource extends RemoteMuzeiArtSource {
     // input: https://i.pximg.net/c/240x480/img-master/img/2017/10/29/00/00/01/65636164_p0_master1200.jpg
     // original: https://i.pximg.net/img-original/img/2017/10/29/00/00/01/65636164_p0.png
     private static final Pattern IMAGE_URI_PATTERN = Pattern.compile(
-        "^(https?://.+?/)c/[0-9]+x[0-9]+/img-master(/.+)_master.+$"
+            "^(https?://.+?/)c/[0-9]+x[0-9]+/img-master(/.+)_master.+$"
     );
 
     private static final String[] IMAGE_SUFFIXS = {
-        ".png",
-        ".jpg",
-        ".gif",
+            ".png",
+            ".jpg",
+            ".gif",
     };
 
-    private Response findOriginalImageResponseFromOldType(JsonObject content) throws RetryException {
+    private Response findOriginalImageResponseFromOldType(JsonObject content) throws IOException {
         String imageUri = content.getString("url", null);
         if (imageUri == null) {
-            throw new RetryException();
+            throw new IOException("Invalid URL");
         }
         final Matcher m = IMAGE_URI_PATTERN.matcher(imageUri);
         if (!m.matches()) {
             Log.e(LOG_TAG, "Match failed: " + imageUri);
-            throw new RetryException();
+            throw new IOException("Unmatched URL pattern");
         }
         final String base = m.group(1), path = m.group(2);
         for (String suffix : IMAGE_SUFFIXS) {
@@ -361,10 +261,10 @@ public class PixivArtSource extends RemoteMuzeiArtSource {
             } catch (IOException e) {}
         }
         Log.e(LOG_TAG, "Not fount orig image: " + imageUri);
-        throw new RetryException();
+        throw new IOException("Couldn't find original image");
     }
 
-    private Response getOriginalImageResponse(JsonObject content, String referer) throws RetryException {
+    private Response getOriginalImageResponse(JsonObject content, String referer) throws IOException {
         JsonValue single_page = content.get("meta_single_page");
         if (single_page == null) {
             return findOriginalImageResponseFromOldType(content);
@@ -372,11 +272,7 @@ public class PixivArtSource extends RemoteMuzeiArtSource {
         String url;
         url = single_page.asObject().getString("original_image_url", null);
         if (url != null) {
-            try {
-                return sendGetRequest(url, referer);
-            } catch (IOException e) {
-                throw new RetryException();
-            }
+            return sendGetRequest(url, referer);
         }
         JsonArray meta_pages = content.get("meta_pages").asArray();
         JsonObject group = null;
@@ -389,7 +285,7 @@ public class PixivArtSource extends RemoteMuzeiArtSource {
 
         JsonObject urls = group.get("image_urls").asObject();
         if (urls == null) {
-            throw new RetryException();
+            throw new IOException("Couldn't find image_urls");
         }
         for (String size : new String[]{"original", "large", "medium"}) {
             url = urls.getString(size, null);
@@ -399,46 +295,42 @@ public class PixivArtSource extends RemoteMuzeiArtSource {
                 } catch (IOException e) {}
             }
         }
-        throw new RetryException();
+        throw new IOException("Couldn't find image groups");
     }
 
     private Uri downloadOriginalImage(final JsonObject content,
                                       final String token,
-                                      final String referer) throws RetryException {
-        final Application app = getApplication();
-        if (app == null) {
-            Log.e(LOG_TAG, "getApplication() returns null");
-            throw new RetryException();
-        }
+                                      final String referer) throws IOException {
+        Context context = getContext();
 
-        final File originalFile = new File(app.getExternalCacheDir(), token);
-
+        final File originalFile;
         try {
-            Response resp = getOriginalImageResponse(content, referer);
-
-            final int status = resp.code();
-            Log.d(LOG_TAG, "Response code: " + status);
-            if (!resp.isSuccessful()) {
-                throw new RetryException();
-            }
-
-            final FileOutputStream fileStream = new FileOutputStream(originalFile);
-            final InputStream inputStream = resp.body().byteStream();
-            try {
-                final byte[] buffer = new byte[1024 * 50];
-                int read;
-                while ((read = inputStream.read(buffer)) > 0) {
-                    fileStream.write(buffer, 0, read);
-                }
-            } finally {
-                fileStream.close();
-            }
-            inputStream.close();
-        } catch (final IOException e) {
-            Log.e(LOG_TAG, e.toString(), e);
-            throw new RetryException();
+            originalFile = new File(context.getCacheDir(), token);
+        } catch (NullPointerException e) {
+            // TODO
+            throw new IOException("Couldn't get cache directory");
         }
 
+        Response resp = getOriginalImageResponse(content, referer);
+
+        final int status = resp.code();
+        Log.d(LOG_TAG, "Response code: " + status);
+        if (!resp.isSuccessful()) {
+            throw new IOException("Unsuccessful request: " + status);
+        }
+
+        final FileOutputStream fileStream = new FileOutputStream(originalFile);
+        final InputStream inputStream = resp.body().byteStream();
+        try {
+            final byte[] buffer = new byte[1024 * 50];
+            int read;
+            while ((read = inputStream.read(buffer)) > 0) {
+                fileStream.write(buffer, 0, read);
+            }
+        } finally {
+            fileStream.close();
+        }
+        inputStream.close();
         Log.d(LOG_TAG, "cache file path: " + originalFile.getAbsolutePath());
         return Uri.parse("file://" + originalFile.getAbsolutePath());
     }
